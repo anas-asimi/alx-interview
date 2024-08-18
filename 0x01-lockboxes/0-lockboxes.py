d2b80360f771c142ac33ba084336c5170f57a521
Anas Asimi <anas2020cr7@gmail.com>
2024-08-18 23:31:08 +0100

    project 0x01-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def unlockBox(box, boxes):
+    box["isOpened"] = True
+    for key in box["keys"]:
+        if boxes[key] and not boxes[key]["isOpened"]:
+            unlockBox(boxes[key], boxes)
+
+
+def canUnlockAll(boxes):
+    boxes = [{"keys": x, "isOpened": False} for x in boxes]
+    unlockBox(boxes[0], boxes)
+    for box in boxes:
+        if not box["isOpened"]:
+            return False
+    return True
