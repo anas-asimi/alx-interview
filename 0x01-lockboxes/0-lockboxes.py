3e96abda51b007cf05aa6b74c9f9846411caaa0c
Anas Asimi <anas2020cr7@gmail.com>
2024-08-18 23:45:43 +0100

    add index checking

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,11 +13,19 @@ def unlockBox(box, boxes):
     """
     box["isOpened"] = True
     for key in box["keys"]:
-        if boxes[key] and not boxes[key]["isOpened"]:
+        if key < len(boxes) and not boxes[key]["isOpened"]:
             unlockBox(boxes[key], boxes)
 
 
 def canUnlockAll(boxes):
+    """
+    canUnlockAll
+    Args:
+        boxes (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     boxes = [{"keys": x, "isOpened": False} for x in boxes]
     unlockBox(boxes[0], boxes)
     for box in boxes:
