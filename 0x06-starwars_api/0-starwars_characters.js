7e00e4c2edcc64e0d44e4dde9ba97301c6f8cf19
Anas Asimi <anas2020cr7@gmail.com>
2024-09-23 20:20:03 +0100

    complete 0x06-starwars_api project

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const request = require("request");
+const process = require("process");
+
+// Get the first argument
+const firstArgument = process.argv[2];
+const endPoint = "https://swapi-api.alx-tools.com/api/films";
+
+// Check if the argument is a number using isNaN()
+if (isNaN(firstArgument) || firstArgument == 0) {
+	process.exit();
+}
+
+request(endPoint, function (error, response, body) {
+	if (error) {
+		console.error("error:", error);
+		console.log("statusCode:", response && response.statusCode);
+	}
+
+	let rep = JSON.parse(body);
+	let film = rep.results[firstArgument - 1];
+	let charactersUrls = film.characters;
+
+	let characters = [];
+
+	for (let i = 0; i < charactersUrls.length; i++) {
+		request(charactersUrls[i], function (error, response, body) {
+			let rep = JSON.parse(body);
+			characters.push({
+				name: rep.name,
+				id: i,
+			});
+		});
+	}
+
+	let ref;
+
+	ref = setInterval(() => {
+		if (characters.length == charactersUrls.length) {
+			// Sort by id
+			characters.sort((a, b) => a.id - b.id);
+			for (let character of characters) console.log(character.name);
+			clearInterval(ref);
+		}
+	}, 250);
+});
