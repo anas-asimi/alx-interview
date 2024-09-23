9e68bbd0c935704e145f1c090092e3386e88b71c
Anas Asimi <anas2020cr7@gmail.com>
2024-09-23 22:25:23 +0100

    fix format

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,47 +1,49 @@
 #!/usr/bin/node
 
-const request = require("request");
-const process = require("process");
+const request = require('request');
+const process = require('process');
 
 // Get the first argument
 const firstArgument = process.argv[2];
-const endPoint = "https://swapi-api.alx-tools.com/api/films";
+const endPoint = 'https://swapi-api.alx-tools.com/api/films';
 
 // Check if the argument is a number using isNaN()
-if (isNaN(firstArgument) || firstArgument == 0) {
-	process.exit();
+if (isNaN(firstArgument) || firstArgument === 0) {
+  process.exit();
 }
 
 request(endPoint, function (error, response, body) {
-	if (error) {
-		console.error("error:", error);
-		console.log("statusCode:", response && response.statusCode);
-	}
+  if (error) {
+    console.error('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+  }
 
-	let rep = JSON.parse(body);
-	let film = rep.results[firstArgument - 1];
-	let charactersUrls = film.characters;
+  const rep = JSON.parse(body);
+  const film = rep.results[firstArgument - 1];
+  const charactersUrls = film.characters;
 
-	let characters = [];
+  const characters = [];
 
-	for (let i = 0; i < charactersUrls.length; i++) {
-		request(charactersUrls[i], function (error, response, body) {
-			let rep = JSON.parse(body);
-			characters.push({
-				name: rep.name,
-				id: i,
-			});
-		});
-	}
+  for (let i = 0; i < charactersUrls.length; i++) {
+    request(charactersUrls[i], function (error, response, body) {
+      if (error) {
+        console.error('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+      }
+      const rep = JSON.parse(body);
+      characters.push({
+        name: rep.name,
+        id: i
+      });
+    });
+  }
 
-	let ref;
-
-	ref = setInterval(() => {
-		if (characters.length == charactersUrls.length) {
-			// Sort by id
-			characters.sort((a, b) => a.id - b.id);
-			for (let character of characters) console.log(character.name);
-			clearInterval(ref);
-		}
-	}, 250);
+  const ref = setInterval(() => {
+    if (characters.length === charactersUrls.length) {
+      // Sort by id
+      characters.sort((a, b) => a.id - b.id);
+      for (const character of characters) console.log(character.name);
+      clearInterval(ref);
+    }
+  }, 250);
 });
