7f610acb3abdffb0f9ae224d98ce2529e7192f2c
Anas Asimi <anas2020cr7@gmail.com>
2024-10-01 21:56:28 +0100

    task 0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,3 +2,48 @@
 """
 0-making_change.py
 """
+from typing import List
+
+
+def findFewestCombination(coins: list[int], total: int, initial: int = 0,  coinsCounter: int = 0) -> int:
+    """
+    findFewestCombination
+    Args:
+        coins (list[int]):
+        total (int):
+        initial (int, optional): Defaults to 0.
+        coinsCounter (int, optional): Defaults to 0.
+    Returns:
+        int:
+    """
+    # print(f'total: {total} initial: {initial}  coinsCounter: {coinsCounter}')
+    if total == initial:
+        return coinsCounter
+
+    for coin in coins:
+        if (initial + coin) > total:
+            continue
+
+        else:
+            # print(f'lets add: {coin}')
+            return findFewestCombination(
+                coins, total, initial+coin, coinsCounter + 1)
+
+
+def makeChange(coins, total: int) -> int:
+    """
+    makeChange
+    Args:
+        coins (_type_):
+        total (int):
+
+    Returns:
+        int:
+    """
+    if total <= 0:
+        return 0
+    sorted_coins = sorted(coins, reverse=True)
+    change = findFewestCombination(sorted_coins, total)
+    if change is not None:
+        return change
+    return -1
